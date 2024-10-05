6b0acbb5ef1d45e8314a29554fe62c319a97b002
DEVYANSHU GAGNEJA <109970405+devyanshugagneja@users.noreply.github.com>
2024-10-06 00:25:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://devyanshugagneja:saharanpur@cluster0.drqy6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect('YOUR MONGO DB LINK', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
